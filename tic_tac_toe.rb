9f39d79327e1f97992547c8fa7257d7b9446bd37
Robert De la Rosa <rdelarosa39@gmail.com>
2021-03-26 21:44:59 -0500

    Feat: column check method created to scale app

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -10,7 +10,9 @@ class TicTacToe
 
     # Step one check for wins in a row for the board
     # row checks
-    p check_rows
+    if check_rows != nil
+      puts check_rows
+    end
     if row1[0] == "o" && row1[1] == "o" && row1[2] == "o"
       return "o"
     end
@@ -36,6 +38,9 @@ class TicTacToe
     end
 
     # column checks
+    if check_columns != nil
+      puts check_columns
+    end
     if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
       return "o"
     end
@@ -80,6 +85,7 @@ class TicTacToe
     return "draw"
   end
 
+  # checks for horizontal wins on a scalable board
   def check_rows
     row_winner = nil
 
@@ -94,5 +100,43 @@ class TicTacToe
     end
 
     row_winner
+  end
+
+  # checks for vertical wins on a scalable board
+  def check_columns
+    column_winner = nil
+
+    # index in arrays will be representations of board column
+    x_arr =[]
+    o_arr =[]
+    # arrays will be the size of the board default value for each column will be 0
+    @board.size.times do
+      x_arr << 0
+      o_arr << 0
+    end
+
+    # updates amount in x or o array as a column based on marker = x or o
+    @board.each do |row|
+      row.each_with_index do |marker, index|
+        if marker == 'x'
+          x_arr[index] += 1
+          # checks if winner to break out of loop
+          if x_arr[index] == @board.size
+            column_winner = 'x'
+            break
+          end
+        elsif marker == 'o'
+          o_arr[index] += 1
+          # checks if winner to break out of loop
+          if o_arr[index] == @board.size
+            column_winner = 'o'
+            break
+          end
+        end
+
+      end
+    end
+
+    column_winner
   end
 end
