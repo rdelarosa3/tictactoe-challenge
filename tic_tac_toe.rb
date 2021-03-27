6ca75530ee7d223bb64ead29beb8efd864e3c606
Robert De la Rosa <rdelarosa39@gmail.com>
2021-03-26 21:51:23 -0500

    Feat: diagonal check method created for scalable app

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,6 +11,7 @@ class TicTacToe
     # Step one check for wins in a row for the board
     # row checks
     if check_rows != nil
+      puts 'row win'
       puts check_rows
     end
     if row1[0] == "o" && row1[1] == "o" && row1[2] == "o"
@@ -39,6 +40,7 @@ class TicTacToe
 
     # column checks
     if check_columns != nil
+      puts 'column win'
       puts check_columns
     end
     if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
@@ -66,6 +68,10 @@ class TicTacToe
     end
 
     # diagonal checks
+    if check_diagonals != nil
+      puts 'diagonal win'
+      puts check_diagonals
+    end
     if row1[0] == "o" && row2[1] == "o" && row3[2] == "o"
       return "o"
     end
@@ -139,4 +145,41 @@ class TicTacToe
 
     column_winner
   end
+
+  # checks for diagonal wins on a scalable board
+  def check_diagonals
+    diagonal_winner = nil
+    # stores the markers
+    backward_diagonal = ''
+    forward_diagonal = ''
+
+    # for a diagonal to be a winner it takes the row[n], row[n+1], row[n+2] and so on..
+    @board.each_with_index do |row,index|
+
+      # check if left to right diagonal is a winner
+      row.each_with_index do |marker, position|
+        if position == index
+          backward_diagonal += marker
+        end
+      end
+
+      # check if right to left diagonal is a winner
+      reversed_row = row.reverse
+      reversed_row.each_with_index do |marker, position|
+        if position == index
+          forward_diagonal += marker
+        end
+      end
+    end
+
+    # checks iteration count of x or o in strings to find winner
+    if backward_diagonal.count('x') == @board.size || forward_diagonal.count('x') == @board.size
+      diagonal_winner = 'x'
+    elsif backward_diagonal.count('o') == @board.size || forward_diagonal.count('o') == @board.size
+      diagonal_winner = 'o'
+    end
+
+    diagonal_winner
+  end
+
 end
